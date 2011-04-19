be6791f32bcf36be706c09f54bcfe0f49081492b
Matt Owen <mowen@iq.harvard.edu>
2011-04-19 13:19:31 -0400

    Adding unit test changes

---
+++ tests/blogit3.R
@@ -0,0 +1,19 @@
+# blogit3
+# Matt Owen
+# Unit test based on a demo by Kosuke Imai and Olivia Lau
+
+library(bivariate.zelig)
+
+data(sanction)
+
+z.out <- zelig(
+               cbind(import, export) ~ coop + cost + target, 
+               constraint = list(
+                 "1" = c("coop", "cost", "target"),
+                 "2" = c("coop", "cost", "target")
+                 ),
+               model = "blogit", data = sanction
+               )
+
+
+summary(z.out)
