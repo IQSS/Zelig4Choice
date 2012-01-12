f4970e793cdd096914d2c9ada3e5647c7d3e5781
Matt <mattisbusiness@gmail.com>
2012-01-12 13:44:17 -0500

    ZeligMultivariate

--- R/ZeligMultivariate-package.R
+++ R/ZeligMultivariate-package.R
@@ -1,9 +1,9 @@
 #' Bivariate Zelig Models
 #'
 #' \tabular{ll}{
-#' Package: \tab ZeligBivariate\cr
-#' Version: \tab 1.0-3\cr
-#' Date: \tab 2011-12-12\cr
+#' Package: \tab ZeligMultivariate\cr
+#' Version: \tab 1.0-4\cr
+#' Date: \tab 2012-01-12\cr
 #' Depends: \tab Zelig (>= 4.0-6), VGAM (>= 0.8-4)\cr
 #' Suggests: mvtnorm, MCMCpack\cr
 #' License: \tab GPL version 2 or newer\cr
@@ -12,8 +12,8 @@
 #'
 #' Add-on pack for Zelig, containing a bivariate probit and logit regession
 #' 
-#' @name ZeligBivariate-package
-#' @aliases ZeligBivariate
+#' @name ZeligMultivariate-package
+#' @aliases ZeligMultivariate
 #' @docType package
 #' @author Matt Owen \email{mowen@@iq.harvard.edu}, Kosuke Imai, Olivia Lau,
 #'   and Gary King 
